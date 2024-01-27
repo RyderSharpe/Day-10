3e26b40f3f4a6cc0ca0a045f0878cce00977960f
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-01-26 21:58:48 -0800

    Functions with Outputs.py
    Also included is an example for a Docstring.

--- Functions with Outputs.py
+++ Functions with Outputs.py
@@ -20,9 +20,16 @@ def format_name(f_name, l_name):
     # print(f_name.title())
     # print(l_name.title())
 
+        ############ DOCSTRINGS ############
+    """ Take a first and last name and format
+    it to return the title case version of the name.
+    (When you hover over "format_name", you'll see the docstring.)"""
+   
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
+
+format_name()
 
 formated_string = format_name("rYdEr", "SHARPE")
 print(formated_string)
